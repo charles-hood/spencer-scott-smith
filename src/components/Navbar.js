d9a9827183fad552b3cd231fc896a99c9e3f701c
Charles Hood <charles.hood@gmail.com>
2023-02-11 10:30:48 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/ssslogofull.svg";
+import logo from "../img/cursiveSlogo.png";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
